4995f30efabc018de63d32c9d989330f5981deee
Carlos Rocha <MoiRouhs@users.noreply.github.com>
2017-02-20 15:44:58 -0500

    Se agregan las libraries de node a utilizar

---
+++ ejercicio-recogerdatos/js/nwde.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var os = require('os');
+var fs = require('fs');
+var gui = require('nw.gui');
+
+function urlext(url){
+  gui.Shell.openExternal(url);
+};
+var d = path.join(os.homedir(),"Desktop","History");
+
+function crearArchivo(){
+  var fi = new Date();
+  var fechai = fi.getDate() +"-"+ (fi.getMonth()+1) +"-"+fi.getFullYear();
+  return fechai ;
+};
+
+var nomarchivo = crearArchivo()+'.rtf';
+var d2 = path.join(d,nomarchivo) ;
+
+function existex(){
+  if (fs.existsSync(d)) {
+    if (!fs.existsSync(d2)){
+      fs.appendFileSync(d2,'');
+    };
+  }else{
+    fs.mkdirSync(d);
+    fs.appendFileSync(d2);
+  };
+};
